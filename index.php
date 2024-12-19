cdee42182b3a04df47783c446d4de3af0a23d9c1
vorthrak <elricodaru@gmail.com>
2024-12-19 14:54:50 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "<RDS-Endpoint>";
+$username = "<Master-Username>";
+$password = "<Master-Password>";
+$dbname = "<Database-Name>";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
